ed0616f86017bb40c9a61c31a24ce2b2e052b137
Priyanshu-PM <priyanshumahukhaye@gmail.com>
2023-03-27 20:05:52 +0530

    add question done

--- src/pages/Teacher/Quiz/Quiz.jsx
+++ src/pages/Teacher/Quiz/Quiz.jsx
@@ -3,17 +3,20 @@ import { Link } from "react-router-dom";
 import Sidebar from "../../../Components/Sidebar";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import Modal from "./Modal";
 
 const Quiz = () => {
   const [quizId, setQuizId] = useState("");
   const [quizes, setQuizes] = useState([]);
+  const [modal, setShowModal] = useState(false);
+  const [QUESTIONS, setQUESTIONS] = useState();
 
   const [quizData, setQuizData] = useState([]);
 
   let Id = useParams();
 
   const apiKey = process.env.REACT_APP_STUDYAI_API;
-  const quizkey = `${apiKey}/quiz/${quizId}`;
+  const quizKey = `${apiKey}/quiz/${quizId}`;
   const quizDemo = `${apiKey}/quiz/63fa00bff48312e9af983087`;
 
   useEffect(() => {
@@ -22,15 +25,21 @@ const Quiz = () => {
   }, [quizDemo]);
   console.log(Id.quizid);
 
+  var teacherData = sessionStorage.getItem("teacher");
+  const tdata = JSON.parse(teacherData);
+
+  console.log(tdata.teacher._id);
+
   const getQuizes = async () => {
     try {
       const { data } = await axios.get(quizDemo);
-      const res = JSON.parse(data.data)
-      setQuizData(res)
-      setQuizes(res.questions)
-      console.log(quizes)
+      const res = JSON.parse(data.data);
+      setQuizData(res);
+
+      setQUESTIONS(quizData.questions);
+      setQuizes(res);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
   // useEffect(() => {
@@ -50,84 +59,141 @@ const Quiz = () => {
   //     });
   // }, [quizDemo]);
 
-  const questions = [
-    {
-      id: 1,
-      question: "What is the capital of France?",
-      answers: [
-        { id: "a", text: "London" },
-        { id: "b", text: "Paris" },
-        { id: "c", text: "Madrid" },
-      ],
-      correctAnswer: "b",
-      selectedAnswer: null,
-    },
-    {
-      id: 2,
-      question: "What is the largest planet in our solar system?",
-      answers: [
-        { id: "a", text: "Jupiter" },
-        { id: "b", text: "Earth" },
-        { id: "c", text: "Saturn" },
-      ],
-      correctAnswer: "a",
-      selectedAnswer: null,
-    },
-    {
-      id: 3,
-      question: "What is the smallest country in the world?",
-      answers: [
-        { id: "a", text: "Monaco" },
-        { id: "b", text: "Nauru" },
-        { id: "c", text: "Vatican City" },
-      ],
-      correctAnswer: "c",
-      selectedAnswer: null,
-    },
-  ];
-
-  // Define a state variable to hold the quiz score
-  const [score, setScore] = useState(0);
-
   const handleSubmit = (event) => {
-    let newScore = 0;
-    questions.forEach((question) => {
-      if (question.selectedAnswer === question.correctAnswer) {
-        newScore++;
-      }
-    });
-
-    setScore(newScore);
-    console.log(questions);
+    console.log(QUESTIONS);
 
     event.preventDefault();
-
-    // axios
-    //   .post(submitquizKey, {
-    //     student: student.student._id,
-    //     quiz: quizid,
-    //     answers: [],
-    //   })
-    //   .then((res) => {
-    //     const data = res.data;
-    //     console.log(data);
-
-    //     if (data.success) {
-    //       console.log("Quiz submitted successfully");
-    //     } else {
-    //       alert("Failed to submit quiz");
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     alert(err);
-    //     console.log(err);
-    //   });
   };
 
-  // console.log(quizData.title);
-  // console.log(quizData.questions);
+  const [questext, setQuestion] = useState("");
+  const [options, setOptions] = useState(["", "", "", ""]);
+  const [correctAnswer, setCorrectAnswer] = useState("");
+
+  const handleQuestionChange = (e) => {
+    setQuestion(e.target.value);
+  };
+
+  const handleOptionChange = ( index, event) => {
+
+    const newOptions = [...options];
+    newOptions[index] = event.target.value;
+    setOptions(newOptions);
+
+  };
+
+  // console.log(tdata.tokem);
+
+  const handleCorrectAnswerChange = (e) => {
+    setCorrectAnswer(e.target.value);
+  };
+
+  const handleaddQuestion = (event) => {
+    event.preventDefault();
+
+    axios
+      .patch(
+        quizKey,
+        {
+          text: questext,
+          options: [],
+          correctAnswer: correctAnswer,
+        },
+        {
+          headers: {
+            Authorization: `${tdata.tokem}`,
+          },
+        }
+      )
+      .then((res) => {
+        const data = res.data;
+        console.log(data);
+
+        if (data.success) {
+          console.log("Question added successfully");
+          setShowModal(false);
+          
+        } else {
+          alert("Failed to add question");
+        }
+      })
+      .catch((err) => {
+        alert(err);
+        console.log(err);
+      });
+  };
+
+  console.log("=====================");
+  console.log(quizData.title);
+  console.log(quizData.questions);
+  console.log("=====================");
+  console.log(QUESTIONS);
+  console.log("=====================");
 
   return (
+    <div>
+    {
+      modal ? (
+        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <form
+            onSubmit={handleaddQuestion}
+            className="max-w-md mx-auto bg-gray-200 shadow-2xl p-10 rounded-md"
+          >
+            <div className="my-4">
+              <label
+                htmlFor="question"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                Question
+              </label>
+              <textarea
+                id="question"
+                className="w-full border-gray-300 rounded-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                value={questext}
+                onChange={(e) => setQuestion(e.target.value)}
+              />
+            </div>
+            <div className="my-4">
+              <label className="block text-gray-700 font-medium mb-2">
+                Options
+              </label>
+              {options.map((option, index) => (
+                <input
+                  key={index}
+                  type="text"
+                  className="w-full border-gray-300 rounded-lg mb-2 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                  placeholder={`Option ${index + 1}`}
+                  value={options[index]}
+                  onChange={(e) => handleOptionChange(index, e)}
+                />
+              ))}
+            </div>
+            <div className="my-4">
+              <label
+                htmlFor="correctAnswer"
+                className="block text-gray-700 font-medium mb-2"
+              >
+                Correct Answer
+              </label>
+              <input
+                id="correctAnswer"
+                type="text"
+                className="w-full border-gray-300 rounded-lg focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                value={correctAnswer}
+                onChange={(e) => setCorrectAnswer(e.target.value)}
+              />
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 text-white font-medium rounded-lg transition-colors duration-300"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      ) : null
+    }
     <div className="bg-[#F3F8FF] min-h-screen ">
       <div className="grid grid-cols-11">
         <div
@@ -137,67 +203,45 @@ text-[#9696a6] min-h-screen fixed w-[18%]"
           <Sidebar />
         </div>
 
-        <div className="col-start-3 col-span-full first-letter:w-full bg-gray-100 min-h-screen py-8">
+        <div className="col-start-3 col-span-full first-letter:w-full bg-gray-100 min-h-screen py-8 pt-0">
           <div className="mx-auto px-4">
-            <div className="bg-white rounded-lg shadow-lg p-8 mb-8">
-              <div>
-                <h1 className="text-3xl font-bold text-gray-800 mb-8">
-                  {quizData.title}
-                </h1>
-              </div>
-              {console.log("inside map ", quizes)};
-              <div>
-                {quizes.map((question, index) => (
-                  // <div key={question.id} className="mb-6">
-                  //   <h2 className="text-xl font-bold text-gray-800 mb-4">
-                  //     {index + 1}.{question.text}
-                  //   </h2>
-                  //   {question.options.map((answer) => (
-                  //     <div className="flex flex-col" key={answer.id}>
-                  //       <input
-                  //         className="form-radio h-4 w-4"
-                  //         type="radio"
-                  //         id={answer.id}
-                  //         name={`question-${question.id}`}
-                  //         value={answer.id}
-                  //         checked={question.selectedAnswer === answer.id}
-                  //         onChange={() =>
-                  //           // handleAnswerSelect(question.id, answer.id)
-                  //           {}
-                  //         }
-                  //       />
-                  //       <label
-                  //         className="inline-flex items-center mb-2"
-                  //         htmlFor={answer.id}
-                  //       >
-                  //         {answer.text}
-                  //       </label>
-                  //     </div>
-                  //   ))}
-                  // </div>
-                  <>
+            {(modal === true) & (<div className="z-50"></div>)}
 
-                  </>
-                ))}
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full float-center"
-                  onClick={handleSubmit}
-                >
-                  Submit Quiz
-                </button>
-                {score > 0 && (
-                  <div>
-                    <h2>Your Score</h2>
-                    <p>
-                      {score} out of {questions.length}
-                    </p>
-                  </div>
-                )}
+            <div>
+              <h1 className="text-3xl font-bold text-gray-800 mb-8">
+                {quizData.title}
+              </h1>
+            </div>
+
+            <div>
+
+
+            </div>
+            <div className="bg-white rounded-lg shadow-lg p-8 mb-8">
+              {console.log(quizData.questions)}
+              <div className="flex flex-end justify-end">
+                <div>{console.log(quizData.questions)}</div>
+                <div>
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full float-center"
+                    onClick={() => 
+                      setShowModal(true)}
+                  >
+                    Add Question
+                  </button>
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full float-center"
+                    onClick={handleSubmit}
+                  >
+                    Get Responses
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
